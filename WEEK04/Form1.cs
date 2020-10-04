9dc70cf22114890351953decf0ec895d1474365e
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-10-04 15:18:38 +0200

    Output formatting function in the CreateTable void.

--- WEEK04/Form1.cs
+++ WEEK04/Form1.cs
@@ -72,7 +72,15 @@ namespace WEEK04
             xlSheet.get_Range(
               GetCell(2, 1),
               GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
-       
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
 
 
