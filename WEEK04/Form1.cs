dd4ff3746d3d059cfb046ddd5400bc583311c7e1
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-10-04 11:36:55 +0200

    Data loaded.

--- WEEK04/Form1.cs
+++ WEEK04/Form1.cs
@@ -12,9 +12,18 @@ namespace WEEK04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            
         }
-    }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+
+     }
 }
