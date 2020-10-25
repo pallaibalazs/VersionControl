f7ee7fe6419d5138cf79d32063b3298144a85165
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-10-25 17:46:06 +0100

    Classes created.

---
+++ WEEK07/WEEK07/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEEK07.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double DeathP { get; set; }
+    }
+}
