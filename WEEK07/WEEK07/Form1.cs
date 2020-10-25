1a5cbc8315ba9b7901b592d20e5450ea07652d85
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-10-25 18:30:01 +0100

    Simulation step made.

--- WEEK07/WEEK07/Form1.cs
+++ WEEK07/WEEK07/Form1.cs
@@ -27,7 +27,27 @@ namespace WEEK07
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbability(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbability(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+          
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep();
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
+
+        
 
         public List<Person> GetPopulation(string csvpath)
         {
@@ -90,6 +110,42 @@ namespace WEEK07
             }
 
             return deathProbabilities;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            
+            if (!person.IsAlive) return;
+
+            
+            byte age = (byte)(year - person.BirthYear);
+
+            
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.DeathP).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
