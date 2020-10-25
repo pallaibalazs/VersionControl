82a3e726f61aece7f9f2f1bbac267b1d5adb2b34
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-10-25 18:03:00 +0100

    Random number generated.

--- WEEK07/WEEK07/Form1.cs
+++ WEEK07/WEEK07/Form1.cs
@@ -18,6 +18,8 @@ namespace WEEK07
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
