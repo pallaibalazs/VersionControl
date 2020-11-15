21dd616b0006de2547d6231a1629715c06f4afb9
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-11-15 19:26:21 +0100

    Ball factory created.

--- WEEK08/WEEK08/Entities/BallFactory.cs
+++ WEEK08/WEEK08/Entities/BallFactory.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace WEEK08.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
     }
 }
