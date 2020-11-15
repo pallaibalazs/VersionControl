5e4675889becf6b668f62a9f6bdffb7e33e594eb
Balázs István Pallai <balazs.pallai@stud.uni-corvinus.hu>
2020-11-15 19:50:51 +0100

    Toy abstraction created.

---
+++ WEEK08/WEEK08/Abstractions/Toy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Label = System.Windows.Forms.Label;
+
+namespace WEEK08.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+       
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
